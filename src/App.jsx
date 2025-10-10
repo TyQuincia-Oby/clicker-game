d19db22f43f58aaf58086c2d0a3a17d1bfd7d7d3
TyQuincia Oby <tyquinciaoby@gmail.com>
2025-10-10 15:07:16 -0500

    Level 7: Random Apple size [complete]

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 
+function randomNumber(a,b){
+  return Math.floor(Math.random() * (b-a) + a);
+}
 function App() {
   const [score, setScore] = useState(0)
   const [lives, setLives] = useState(3)
+  const [appleSize, setappleSize] = useState(100)
+  const appleStyle = {
+    width: appleSize + "px",
+    height: appleSize + "px"
+  }
 
   function clickTarget(event){
     console.log("Apple clicked")
     setScore(score + 1);
     event.stopPropagation();
+    randomSize();
   }
 
   function missTarget(){
     console.log("Background clicked")
     setLives(lives - 1);
+    randomSize();
+  }
 
-    
+  function randomSize(){
+    const randomSize = randomNumber(20,100);
+    setappleSize(randomSize);
   }
 
   return (
@@ -27,7 +40,7 @@ function App() {
           <p>Lives: {lives} </p>
         </div>
         <div className="orchard-background" onClick={missTarget}>
-          <div className="apple-target" onClick={clickTarget}>
+          <div className="apple-target" onClick={clickTarget} style={appleStyle}>
 
           </div>
         </div>
