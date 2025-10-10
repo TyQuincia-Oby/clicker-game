9de4b32d9c72385f50f263e95bddf079e6a712c9
TyQuincia Oby <tyquinciaoby@gmail.com>
2025-10-10 13:29:45 -0500

    Level 5: Add Score Tracking [complete]

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0)
+
+  function clickTarget(){
+    setScore(score + 1);
+    {/*clickTarget.stopPropagation(); -make sure this is correct, then uncomment*/} 
+  
+  }
 
   return (
     <>
-      <div>
-        <h1>Let's Get Pickin'!!</h1>
+      <div className="full-page">
+        <div className="stats">
+          <h1>Let's Get Pickin'!!</h1>
+          
+          <p>Score: {score} </p>
+        </div>
         <div className="orchard-background">
-          <div className="apple-target">
-            
+          <div className="apple-target" onClick={clickTarget}>
+
           </div>
         </div>
       </div>
